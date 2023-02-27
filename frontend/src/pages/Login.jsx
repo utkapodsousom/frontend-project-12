2dafead9c98a9ccb48e88b1c436e3b8cc856ec9d
utkapodsousom <utkapodsousom@gmail.com>
2023-02-27 16:20:38 +0300

    add login form, login post request and get JWT token

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,5 @@
-import { Formik, Form, Field } from "formik";
+import { Formik, Field, ErrorMessage, Form } from "formik";
+import axios from "axios";
 import * as yup from "yup";
 
 const loginSchema = yup.object().shape({
@@ -9,28 +10,59 @@ const loginSchema = yup.object().shape({
     .required("Required"),
   password: yup
     .string()
-    .min(8, "Too Short!")
+    .min(5, "Too Short!")
     .max(50, "Too Long!")
     .required("Required"),
 });
 
 const Login = () => {
   return (
-    <div>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        validationSchema={loginSchema}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
-      >
+    <Formik
+      initialValues={{ username: "", password: "", confirmPassword: "" }}
+      validationSchema={loginSchema}
+      onSubmit={(values) => {
+        axios
+          .post("/api/v1/login", {
+            username: values.username,
+            password: values.password,
+          })
+          .then((res) => {
+            const { token, username } = res.data;
+            localStorage.setItem("token", token);
+            localStorage.setItem("login", username);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }}
+    >
+      {({ errors }) => (
         <Form>
-          <Field type="username" name="username" />
-          <Field type="password" name="password" />
-          <button type="submit">Submit</button>
+          <h1>Logig Page</h1>
+          <div>
+            <Field type="username" name="username" placeholder="Login" />
+            <label htmlFor="username">
+              Username:
+            </label>
+            <ErrorMessage name="username">
+              {() => <div>{errors.username}</div>}
+            </ErrorMessage>
+          </div>
+          <div>
+            <Field type="password" name="password" />
+            <label htmlFor="password">
+              Password:
+            </label>
+            <ErrorMessage name="password">
+              {() => <div>{errors.password}</div>}
+            </ErrorMessage>
+          </div>
+          <button type="submit">
+            Login
+          </button>
         </Form>
-      </Formik>
-    </div>
+      )}
+    </Formik>
   );
 };
 
