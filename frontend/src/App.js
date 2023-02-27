b556112dbfe1b446cb6acfc368afcca9fd523f39
utkapodsousom <utkapodsousom@gmail.com>
2023-02-27 20:48:38 +0300

    add context for auth state, add redirect based on login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,49 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main';
-import Error from './pages/Error';
-import Login from './pages/Login';
+import { createContext, useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import Main from "./pages/Main";
+import Error from "./pages/Error";
+import Login from "./pages/Login";
+
+export const AuthContext = createContext({});
+
+const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    setToken(localStorage.getItem("token"));
+  }, []);
+
+  const login = async (values) => {
+    try {
+      const response = await axios.post("/api/v1/login", values);
+      const { token: loginToken, username } = response.data;
+      setToken(loginToken);
+      localStorage.setItem("token", loginToken);
+      localStorage.setItem("login", username);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
-import { Formik, Field, ErrorMessage, Form } from "formik";
-import axios from "axios";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
 import * as yup from "yup";
+import { AuthContext } from "../App";
 
 const loginSchema = yup.object().shape({
   username: yup
@@ -16,6 +18,28 @@ const loginSchema = yup.object().shape({
 });
 
 const Login = () => {
+  const { login, token } = useContext(AuthContext);
+  console.log(login);
+  const navigate = useNavigate();
+
+  const onSubmit = (values) => {
+    login(values);
+  };
+
+  const formik = useFormik({
+    initialValues: { username: "", password: "" },
+    validationSchema: loginSchema,
+    onSubmit,
+  });
+
+  console.log(formik);
+
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [navigate, token]);
+
   return (
     <div className="dark:bg-slate-700 flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
@@ -24,70 +48,56 @@ const Login = () => {
             Sign in to your account
           </h2>
         </div>
-        <Formik
-          initialValues={{ username: "", password: "", confirmPassword: "" }}
-          validationSchema={loginSchema}
-          onSubmit={(values) => {
-            axios
-              .post("/api/v1/login", {
-                username: values.username,
-                password: values.password,
-              })
-              .then((res) => {
-                const { token, username } = res.data;
-                localStorage.setItem("token", token);
-                localStorage.setItem("login", username);
-              })
-              .catch((error) => {
-                console.log(error);
-              });
-          }}
-        >
-          {({ errors }) => (
-            <Form className="mt-8 space-y-6" action="#" method="POST">
-              <div className="rounded-md shadow-sm grid grid-cols-1 gap-6">
-                <div className="relative">
-                  <label htmlFor="username" className="block text-md font-medium text-gray-700 dark:text-white">
-                    Username:
-                  </label>
-                  <Field
-                    className="relative block w-full appearance-none rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-md invalid:border-pink-400 invalid:text-pink-500"
-                    type="username"
-                    name="username"
-                    placeholder="Username"
-                    required
-                  />
-                  <ErrorMessage name="username">
-                    {() => <div className="absolute peer-invalid:visible text-pink-500 font-medium">{errors.username}</div>}
-                  </ErrorMessage>
-                </div>
-                <div className="relative">
-                  <label htmlFor="password" className="block text-md font-medium text-gray-700 dark:text-white">
-                    Password:
-                  </label>
-                  <Field
-                    className="relative block w-full appearance-none rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-md  invalid:border-pink-400 invalid:text-pink-500"
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    required
-                  />
-                  <ErrorMessage name="password">
-                    {() => <div className="absolute peer-invalid:visible text-pink-500 font-medium">{errors.password}</div>}
-                  </ErrorMessage>
-                </div>
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-md font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Sign in
-                </button>
-              </div>
-            </Form>
-          )}
-        </Formik>
+        <form className="mt-8 space-y-6" onSubmit={formik.handleSubmit} method="POST">
+          <div className="rounded-md shadow-sm grid grid-cols-1 gap-6">
+            <div className="relative">
+              <label
+                htmlFor="username"
+                className="block text-md font-medium text-gray-700 dark:text-white"
+              >
+                Username:
+              </label>
+              <input
+                className="relative block w-full appearance-none rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-md invalid:border-pink-400 invalid:text-pink-500"
+                type="text"
+                name="username"
+                placeholder="Username"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.username}
+                required
+              />
+              {formik.touched.username && formik.errors.username ? <div className="absolute peer-invalid:visible text-pink-500 font-medium">{formik.errors.username}</div> : null}
+            </div>
+            <div className="relative">
+              <label
+                htmlFor="password"
+                className="block text-md font-medium text-gray-700 dark:text-white"
+              >
+                Password:
+              </label>
+              <input
+                className="relative block w-full appearance-none rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-md  invalid:border-pink-400 invalid:text-pink-500"
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+                required
+              />
+              {formik.touched.password && formik.errors.password ? <div className="absolute peer-invalid:visible text-pink-500 font-medium">{formik.errors.password}</div> : null}
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-md font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
